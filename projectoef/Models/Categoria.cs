b54de098e2ad89ffeb278b3cd41e53404bcb54eb
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-15 08:36:34 -0500

    Creacion de los modelos de Categoria y Tarea

---
+++ projectoef/Models/Categoria.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projectoef.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Tarea> Tareas { get; set; }
+    }
+}
