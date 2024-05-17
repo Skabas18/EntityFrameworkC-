a65b7444583a61317f52bc89952492cde9954e66
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-17 08:54:05 -0500

    Creacion del modelo de tarea con Fluent API

--- projectoef/TareasContext.cs
+++ projectoef/TareasContext.cs
@@ -13,5 +13,14 @@ namespace proyectoef
         public DbSet<Tarea> Tareas { get; set; }
 
         public TareasContext(DbContextOptions<TareasContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Categoria>(categoria =>{
+                categoria.ToTable("Categoria");
+                categoria.HasKey(p => p.CategoriaId);
+                categoria.Property(p =>p.Nombre).IsRequired().HasMaxLength(150);
+                categoria.Property(p =>p.Descripcion);
+            });
+        }
     }
 }
