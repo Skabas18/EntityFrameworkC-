c33c933f3453a1cc9d6a38cfc809126f6250540f
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-17 08:06:17 -0500

    Creación de la conexion a la base de datos postgresql

--- projectoef/Program.cs
+++ projectoef/Program.cs
@@ -3,10 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using proyectoef;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddNpgsql<TareasContext>(builder.Configuration.GetConnectionString("CnTareasDb"));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); 
 
 app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
     dbContext.Database.EnsureCreated();
