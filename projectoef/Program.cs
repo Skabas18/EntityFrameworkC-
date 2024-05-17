6ea4b3a9bb0b4c8a32cfd06a656635e19549a60a
Sebastian Cabal <sebascabalq@gmail.com>
2024-05-17 15:55:12 -0500

    Guardando datos con Entity Framework

--- projectoef/Program.cs
+++ projectoef/Program.cs
@@ -1,21 +1,47 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
+
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddNpgsql<TareasContext>(builder.Configuration.GetConnectionString("CnTareasDb"));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!"); 
+app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/dbconexion", ([FromServices] TareasContext dbContext) => {
-    dbContext.Database.EnsureCreated();
-    return Results.Ok("Base de datos en memoria: "+ dbContext.Database.IsInMemory());
-});
+app.MapGet(
+    "/dbconexion",
+    ([FromServices] TareasContext dbContext) =>
+    {
+        dbContext.Database.EnsureCreated();
+        return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    }
+);
 
-app.MapGet("/api/tareas", ([FromServices] TareasContext dbContext) => {
-    return Results.Ok(dbContext.Tareas.Include(p =>p.Categoria).Where(p => p.PrioridadTarea == proyectoef.Models.Prioridad.Baja));
-});
+app.MapGet(
+    "/api/tareas",
+    ([FromServices] TareasContext dbContext) =>
+    {
+        return Results.Ok(
+            dbContext
+                .Tareas.Include(p => p.Categoria)
+                // .Where(p => p.PrioridadTarea == proyectoef.Models.Prioridad.Baja)
+        );
+    }
+);
 
+app.MapPost(
+    "/api/tareas",
+    async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
+    {
+        tarea.TareaId = Guid.NewGuid();
+        tarea.FechaCreacion = DateTime.Now;
+        await dbContext.AddAsync(tarea);
+        // await dbContext.Tareas.AddAsync(tarea);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+);
 app.Run();
